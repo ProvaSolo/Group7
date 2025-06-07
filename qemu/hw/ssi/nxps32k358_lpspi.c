b20e474a07499fce0a4b7a5fe58d0d2ae789ff58
Vitocuc <vitocucinelli05@gmail.com>
2025-06-07 20:12:18 +0200

    Modifies for lpspsi + Fifo implementation

--- qemu/hw/ssi/nxps32k358_lpspi.c
+++ qemu/hw/ssi/nxps32k358_lpspi.c
@@ -1,122 +1,183 @@
 #include "qemu/osdep.h"
 #include "qemu/log.h"
 #include "qemu/module.h"
-#include "hw/ssi/nxps32k358_lpspi.h"
 #include "migration/vmstate.h"
+#include "hw/irq.h"
+#include "hw/qdev-properties.h"
+#include "hw/ssi/ssi.h"
+#include "hw/ssi/nxps32k358_lpspi.h"
 
 #ifndef NXP_LPSPI_ERR_DEBUG
-#define NXP_LPSPI_ERR_DEBUG 0
+#define NXP_LPSPI_ERR_DEBUG 1
 #endif
 
-// REVISONE 1 -- 20 MAY
-#define DB_PRINT_L(lvl, fmt, args...)               \
-    do                                              \
-    {                                               \
-        if (NXP_LPSPI_ERR_DEBUG >= lvl)             \
-        {                                           \
+#define DB_PRINT_L(lvl, fmt, args...)      \
+    do {                                   \
+        if (NXP_LPSPI_ERR_DEBUG >= lvl) {  \
             qemu_log("%s: " fmt, __func__, ##args); \
-        }                                           \
+        }                                  \
     } while (0)
 
 #define DB_PRINT(fmt, args...) DB_PRINT_L(1, fmt, ##args)
 
-// Modificare i reset, per reference a pagina 2862 del Reference Manual
+static void lpspi_update_status(NXPS32K358LPSPIState *s)
+{
+    uint8_t tx_word_count = fifo8_num_used(&s->tx_fifo) / 4;
+    uint8_t rx_word_count = fifo8_num_used(&s->rx_fifo) / 4;
+
+    s->lpspi_fsr = (rx_word_count << 16) | (tx_word_count << 0);
+
+    if (fifo8_num_free(&s->tx_fifo) >= 4) {
+        s->lpspi_sr |= LPSPI_SR_TDF;
+    } else {
+        s->lpspi_sr &= ~LPSPI_SR_TDF;
+    }
+
+    if (fifo8_num_used(&s->rx_fifo) >= 4) {
+        s->lpspi_sr |= LPSPI_SR_RDF;
+    } else {
+        s->lpspi_sr &= ~LPSPI_SR_RDF;
+    }
+    
+    if (rx_word_count == 0) {
+        s->lpspi_rsr |= LPSPI_RSR_RXEMPTY;
+    } else {
+        s->lpspi_rsr &= ~LPSPI_RSR_RXEMPTY;
+    }
+}
+
+static void lpspi_update_irq(NXPS32K358LPSPIState *s)
+{
+    lpspi_update_status(s);
+    if ((s->lpspi_sr & s->lpspi_ier) & (LPSPI_SR_TDF | LPSPI_SR_RDF)) {
+        qemu_set_irq(s->irq, 1);
+    } else {
+        qemu_set_irq(s->irq, 0);
+    }
+}
+
+// <<< VERSIONE FINALE E CORRETTA DELLA FUNZIONE DI FLUSH >>>
+static void lpspi_flush_txfifo(NXPS32K358LPSPIState *s)
+{
+    // Controlla PRIMA se un trasferimento è possibile.
+    // Se non c'è una parola intera da inviare O non c'è spazio per riceverla,
+    // non iniziare nemmeno la transazione sul bus (non toccare il CS).
+    if ((fifo8_num_used(&s->tx_fifo) < 4) || (fifo8_num_free(&s->rx_fifo) < 4)) {
+        DB_PRINT("Flush requested, but blocked. TX has %d bytes, RX has %d free.\n",
+                 fifo8_num_used(&s->tx_fifo), fifo8_num_free(&s->rx_fifo));
+        // Aggiorna comunque lo stato, potrebbe essere necessario aggiornare i flag.
+        lpspi_update_irq(s);
+        return;
+    }
+
+    uint8_t pcs = (s->lpspi_tcr & TCR_PCS_MASK) >> TCR_PCS_SHIFT;
+    if (pcs >= s->num_cs_lines) {
+        qemu_log_mask(LOG_GUEST_ERROR, "%s: Invalid Chip Select %d\n", __func__, pcs);
+        return;
+    }
+
+    // Un trasferimento è possibile, quindi inizia la transazione sul bus.
+    DB_PRINT("Asserting CS%d for transfer burst.\n", pcs);
+    qemu_set_irq(s->cs_lines[pcs], 0);
+
+    // Esegui un ciclo e trasferisci tutti i dati possibili in una sola volta.
+    while ((fifo8_num_used(&s->tx_fifo) >= 4) && (fifo8_num_free(&s->rx_fifo) >= 4)) {
+        uint32_t tx_word = 0;
+        tx_word |= (uint32_t)fifo8_pop(&s->tx_fifo);
+        tx_word |= (uint32_t)fifo8_pop(&s->tx_fifo) << 8;
+        tx_word |= (uint32_t)fifo8_pop(&s->tx_fifo) << 16;
+        tx_word |= (uint32_t)fifo8_pop(&s->tx_fifo) << 24;
+        
+        uint32_t rx_word = ssi_transfer(s->ssi, tx_word);
+        
+        fifo8_push(&s->rx_fifo, rx_word & 0xFF);
+        fifo8_push(&s->rx_fifo, (rx_word >> 8) & 0xFF);
+        fifo8_push(&s->rx_fifo, (rx_word >> 16) & 0xFF);
+        fifo8_push(&s->rx_fifo, (rx_word >> 24) & 0xFF);
+    }
+
+    // La transazione sul bus è finita.
+    DB_PRINT("De-asserting CS%d after transfer burst.\n", pcs);
+    qemu_set_irq(s->cs_lines[pcs], 1);
+
+    // Il "messaggio" complessivo è finito? Lo è solo se la TX FIFO è vuota.
+    if (fifo8_is_empty(&s->tx_fifo)) {
+        s->lpspi_sr &= ~LPSPI_SR_MBF;
+        DB_PRINT("TX FIFO is now empty, MBF cleared.\n");
+    }
+
+    lpspi_update_irq(s);
+}
+
+static void nxps32k358_lpspi_do_reset(NXPS32K358LPSPIState *s)
+{
+    s->lpspi_verid = 0x02000004;
+    s->lpspi_param = 0x00080202;
+    s->lpspi_cr = 0x0;
+    s->lpspi_sr = LPSPI_SR_TDF;
+    s->lpspi_ier = 0x0;
+    s->lpspi_der = 0x0;
+    s->lpspi_cfgr0 = 0x0;
+    s->lpspi_cfgr1 = 0x0;
+    s->lpspi_ccr = 0x0;
+    s->lpspi_fcr = 0x0;
+    s->lpspi_fsr = 0x0;
+    s->lpspi_tcr = 0xFFFFFFFF;
+    s->lpspi_tdr = 0x0;
+    s->lpspi_rsr = LPSPI_RSR_RXEMPTY;
+    s->lpspi_rdr = 0x0;
+
+    fifo8_reset(&s->tx_fifo);
+    fifo8_reset(&s->rx_fifo);
+    
+    for (int i = 0; i < s->num_cs_lines; ++i) {
+        qemu_set_irq(s->cs_lines[i], 1);
+    }
+
+    lpspi_update_irq(s);
+}
+
 static void nxps32k358_lpspi_reset(DeviceState *dev)
 {
-    NXPS32K358LPSPIState *s = NXPS32K358_LPSPI(dev);
-
-    s->lpspi_verid = 0x02000004; // Version ID
-    s->lpspi_param = 0x00080202; // Parameter Register, idem
-    s->lpspi_cr = 0x00000000;
-    s->lpspi_sr = 0x00000001; // Di solito TDF=1 dopo reset, controlla RM
-    s->lpspi_ier = 0x00000000;
-    s->lpspi_der = 0x00000000;
-    s->lpspi_cfgr0 = 0x00000000;
-    s->lpspi_cfgr1 = 0x00000000;
-    // s->lpspi_dmr0 = 0x00000000; // Se usi DMR0/1, decommenta
-    // s->lpspi_dmr1 = 0x00000000;
-    s->lpspi_ccr = 0x00000000;
-    s->lpspi_fcr = 0x00000000;
-    s->lpspi_fsr = 0x00000003; // TXCOUNT=0, RXCOUNT=0, TFFF=1, RFFF=1 tipico reset
-    s->lpspi_tcr = 0x0000001F;
-    s->lpspi_tdr = 0x00000000;
-    s->lpspi_rsr = 0x00000002; // RDF=1 tipico reset
-    s->lpspi_rdr = 0x00000000;
-}
-static void nxps32k358_lpspi_transfer(NXPS32K358LPSPIState *s)
-{
-    DB_PRINT("Data to send: 0x%x\n", s->lpspi_tdr);
-
-    // Perform the transfer
-    s->lpspi_rdr = ssi_transfer(s->ssi, s->lpspi_tdr);
-
-    // Update status registers
-    s->lpspi_sr |= (1 << 1); // Set RDF (Receive Data Flag)
-    s->lpspi_sr |= (1 << 0); // Set TDF (Transmit Data Flag)
-
-    // Update FIFO status if needed
-    /*
-    s->lpspi_fsr &= ~(0xF << 16); // Clear RXCOUNT
-    s->lpspi_fsr |= (1 << 16);    // Set RXCOUNT to 1
-    s->lpspi_fsr |= (1 << 0);     // Set RFDF (Receive FIFO Drain Flag)
-    */
-    // If interrupts are enabled, trigger them
-    /*
-    if (s->lpspi_ier & (1 << 1))
-    { // If RDF interrupt enabled
-        qemu_irq_raise(s->irq);
-    }
-    */
-    DB_PRINT("Data received: 0x%x\n", s->lpspi_rdr);
+    nxps32k358_lpspi_do_reset(NXPS32K358_LPSPI(dev));
 }
 
 static uint64_t nxps32k358_lpspi_read(void *opaque, hwaddr addr, unsigned int size)
 {
     NXPS32K358LPSPIState *s = opaque;
+    lpspi_update_status(s);
 
-    DB_PRINT("Address: 0x%" HWADDR_PRIx "\n", addr);
-
-    switch (addr)
-    {
-    case S32K_LPSPI_VERID:
-        return s->lpspi_verid;
-    case S32K_LPSPI_PARAM:
-        return s->lpspi_param;
-    case S32K_LPSPI_CR:
-        return s->lpspi_cr;
-    case S32K_LPSPI_SR:
-        return s->lpspi_sr;
-    case S32K_LPSPI_IER:
-        return s->lpspi_ier;
-    case S32K_LPSPI_DER:
-        return s->lpspi_der;
-    case S32K_LPSPI_CFGR0:
-        return s->lpspi_cfgr0;
-    case S32K_LPSPI_CFGR1:
-        return s->lpspi_cfgr1;
-    // case S32K_LPSPI_DMR0:
-    //     return s->lpspi_dmr0;
-    // case S32K_LPSPI_DMR1:
-    //     return s->lpspi_dmr1;
-    case S32K_LPSPI_CCR:
-        return s->lpspi_ccr;
-    case S32K_LPSPI_FCR:
-        return s->lpspi_fcr;
-    case S32K_LPSPI_FSR:
-        return s->lpspi_fsr;
-    case S32K_LPSPI_TCR:
-        return s->lpspi_tcr;
-    case S32K_LPSPI_TDR:
-        return s->lpspi_tdr;
-    case S32K_LPSPI_RSR:
-        return s->lpspi_rsr;
-    case S32K_LPSPI_RDR:
-        return s->lpspi_rdr;
-    default:
-        qemu_log_mask(LOG_GUEST_ERROR, "%s: Bad offset 0x%" HWADDR_PRIx "\n", __func__, addr);
+    switch (addr) {
+    case S32K_LPSPI_VERID: return s->lpspi_verid;
+    case S32K_LPSPI_PARAM: return s->lpspi_param;
+    case S32K_LPSPI_CR: return s->lpspi_cr;
+    case S32K_LPSPI_SR: return s->lpspi_sr;
+    case S32K_LPSPI_IER: return s->lpspi_ier;
+    case S32K_LPSPI_DER: return s->lpspi_der;
+    case S32K_LPSPI_CFGR0: return s->lpspi_cfgr0;
+    case S32K_LPSPI_CFGR1: return s->lpspi_cfgr1;
+    case S32K_LPSPI_CCR: return s->lpspi_ccr;
+    case S32K_LPSPI_FCR: return s->lpspi_fcr;
+    case S32K_LPSPI_FSR: return s->lpspi_fsr;
+    case S32K_LPSPI_TCR: return s->lpspi_tcr;
+    case S32K_LPSPI_RSR: return s->lpspi_rsr;
+    case S32K_LPSPI_RDR: {
+        if (fifo8_num_used(&s->rx_fifo) < 4) {
+            return 0;
+        }
+        uint32_t ret = 0;
+        ret |= (uint32_t)fifo8_pop(&s->rx_fifo);
+        ret |= (uint32_t)fifo8_pop(&s->rx_fifo) << 8;
+        ret |= (uint32_t)fifo8_pop(&s->rx_fifo) << 16;
+        ret |= (uint32_t)fifo8_pop(&s->rx_fifo) << 24;
+        s->lpspi_rdr = ret;
+        lpspi_flush_txfifo(s);
+        return ret;
     }
-
-    return 0;
+    default:
+        qemu_log_mask(LOG_GUEST_ERROR, "%s: Bad read offset 0x%" HWADDR_PRIx "\n", __func__, addr);
+        return 0;
+    }
 }
 
 static void nxps32k358_lpspi_write(void *opaque, hwaddr addr, uint64_t val64, unsigned int size)
@@ -124,81 +185,91 @@ static void nxps32k358_lpspi_write(void *opaque, hwaddr addr, uint64_t val64, un
     NXPS32K358LPSPIState *s = opaque;
     uint32_t value = val64;
 
-    DB_PRINT("Address: 0x%" HWADDR_PRIx ", Value: 0x%x\n", addr, value);
-
-    switch (addr)
-    {
+    switch (addr) {
     case S32K_LPSPI_VERID:
-        qemu_log_mask(LOG_GUEST_ERROR, "%s: Read only register: 0x%" HWADDR_PRIx "\n", __func__, addr);
-        return;
     case S32K_LPSPI_PARAM:
-        qemu_log_mask(LOG_GUEST_ERROR, "%s: Read only register: 0x%" HWADDR_PRIx "\n", __func__, addr);
-        return;
-    case S32K_LPSPI_CR:
-        s->lpspi_cr = value;
-        return;
-    case S32K_LPSPI_SR:
-        qemu_log_mask(LOG_GUEST_ERROR, "%s: Read only register: 0x%" HWADDR_PRIx "\n", __func__, addr);
-        return;
-    case S32K_LPSPI_IER:
-        s->lpspi_ier = value;
-        return;
-    case S32K_LPSPI_DER:
-        s->lpspi_der = value;
-        return;
-    case S32K_LPSPI_CFGR0:
-        s->lpspi_cfgr0 = value;
-        return;
-    case S32K_LPSPI_CFGR1:
-        s->lpspi_cfgr1 = value;
-        return;
-    // case S32K_LPSPI_DMR0:
-    //     s->lpspi_dmr0 = value;
-    //     return;
-    // case S32K_LPSPI_DMR1:
-    //     s->lpspi_dmr1 = value;
-    //     return;
-    case S32K_LPSPI_CCR:
-        s->lpspi_ccr = value;
-        return;
-    case S32K_LPSPI_FCR:
-        s->lpspi_fcr = value;
-        return;
     case S32K_LPSPI_FSR:
-        qemu_log_mask(LOG_GUEST_ERROR, "%s: Read only register: 0x%" HWADDR_PRIx "\n", __func__, addr);
+    case S32K_LPSPI_RSR:
+    case S32K_LPSPI_RDR:
+        qemu_log_mask(LOG_GUEST_ERROR, "%s: Write to read-only reg 0x%" HWADDR_PRIx "\n", __func__, addr);
         return;
+
+    case S32K_LPSPI_CR:
+        if (value & LPSPI_CR_RST) {
+            nxps32k358_lpspi_do_reset(s);
+            return;
+        }
+        if (value & LPSPI_CR_RSTF) {
+            fifo8_reset(&s->tx_fifo);
+            fifo8_reset(&s->rx_fifo);
+        }
+        s->lpspi_cr = value;
+        break;
+
+    case S32K_LPSPI_SR:
+        s->lpspi_sr &= ~value;
+        break;
+
     case S32K_LPSPI_TCR:
         s->lpspi_tcr = value;
+        if (s->lpspi_cr & LPSPI_CR_MEN) {
+            if (!(s->lpspi_sr & LPSPI_SR_MBF) && !fifo8_is_empty(&s->tx_fifo)) {
+                s->lpspi_sr |= LPSPI_SR_MBF;
+            }
+            lpspi_flush_txfifo(s);
+        }
         return;
+
     case S32K_LPSPI_TDR:
-        s->lpspi_tdr = value;
-        // Optionally trigger transfer logic here
+        if (s->lpspi_cr & LPSPI_CR_MEN) {
+            if (fifo8_num_free(&s->tx_fifo) < 4) {
+                qemu_log_mask(LOG_GUEST_ERROR, "%s: Write to full TX FIFO!\n", __func__);
+            } else {
+                if (!(s->lpspi_sr & LPSPI_SR_MBF)) {
+                    s->lpspi_sr |= LPSPI_SR_MBF;
+                }
+                s->lpspi_tdr = value;
+                fifo8_push(&s->tx_fifo, value & 0xFF);
+                fifo8_push(&s->tx_fifo, (value >> 8) & 0xFF);
+                fifo8_push(&s->tx_fifo, (value >> 16) & 0xFF);
+                fifo8_push(&s->tx_fifo, (value >> 24) & 0xFF);
+            }
+            lpspi_flush_txfifo(s);
+        } else {
+            qemu_log_mask(LOG_GUEST_ERROR, "LPSPI is not enabled, cannot write to TDR\n");
+        }
         return;
-    case S32K_LPSPI_RSR:
-        qemu_log_mask(LOG_GUEST_ERROR, "%s: Read only register: 0x%" HWADDR_PRIx "\n", __func__, addr);
-        return;
-    case S32K_LPSPI_RDR:
-        qemu_log_mask(LOG_GUEST_ERROR, "%s: Read only register: 0x%" HWADDR_PRIx "\n", __func__, addr);
-        return;
+
+    case S32K_LPSPI_IER: s->lpspi_ier = value; break;
+    case S32K_LPSPI_DER: s->lpspi_der = value; break;
+    case S32K_LPSPI_CFGR0: s->lpspi_cfgr0 = value; break;
+    case S32K_LPSPI_CFGR1: s->lpspi_cfgr1 = value; break;
+    case S32K_LPSPI_CCR: s->lpspi_ccr = value; break;
+    case S32K_LPSPI_FCR: s->lpspi_fcr = value; break;
+
     default:
-        qemu_log_mask(LOG_GUEST_ERROR,
-                      "%s: Bad offset 0x%" HWADDR_PRIx "\n", __func__, addr);
+        qemu_log_mask(LOG_GUEST_ERROR, "%s: Bad write offset 0x%" HWADDR_PRIx "\n", __func__, addr);
+        return;
     }
+
+    lpspi_update_irq(s);
 }
 
 static const MemoryRegionOps nxps32k358_lpspi_ops = {
     .read = nxps32k358_lpspi_read,
     .write = nxps32k358_lpspi_write,
-    .endianness = DEVICE_NATIVE_ENDIAN,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+    .impl.min_access_size = 4,
+    .impl.max_access_size = 4,
 };
 
 static const VMStateDescription vmstate_nxps32k358_lpspi = {
     .name = TYPE_NXPS32K358_LPSPI,
-    .version_id = 1,
-    .minimum_version_id = 1,
+    .version_id = 7,
+    .minimum_version_id = 7,
     .fields = (const VMStateField[]){
-
-        // NXP S32K358 LPSPI registers
+        VMSTATE_FIFO8(tx_fifo, NXPS32K358LPSPIState),
+        VMSTATE_FIFO8(rx_fifo, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_verid, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_param, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_cr, NXPS32K358LPSPIState),
@@ -207,8 +278,6 @@ static const VMStateDescription vmstate_nxps32k358_lpspi = {
         VMSTATE_UINT32(lpspi_der, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_cfgr0, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_cfgr1, NXPS32K358LPSPIState),
-        // VMSTATE_UINT32(lpspi_dmr0, NXPS32K358LPSPIState), // Uncomment if used
-        // VMSTATE_UINT32(lpspi_dmr1, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_ccr, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_fcr, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_fsr, NXPS32K358LPSPIState),
@@ -216,28 +285,40 @@ static const VMStateDescription vmstate_nxps32k358_lpspi = {
         VMSTATE_UINT32(lpspi_tdr, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_rsr, NXPS32K358LPSPIState),
         VMSTATE_UINT32(lpspi_rdr, NXPS32K358LPSPIState),
+        VMSTATE_END_OF_LIST()
+    }
+};
 
-        VMSTATE_END_OF_LIST()}};
+static const Property nxps32k358_lpspi_properties[] = {
+    DEFINE_PROP_UINT8("num-cs-lines", NXPS32K358LPSPIState, num_cs_lines, 1),
+};
 
-static void nxps32k358_lpspi_init(Object *obj)
+static void nxps32k358_lpspi_realize(DeviceState *dev, Error **errp)
 {
-    NXPS32K358LPSPIState *s = NXPS32K358_LPSPI(obj);
-    DeviceState *dev = DEVICE(obj);
+    SysBusDevice *sbd = SYS_BUS_DEVICE(dev);
+    NXPS32K358LPSPIState *s = NXPS32K358_LPSPI(dev);
 
-    memory_region_init_io(&s->mmio, obj, &nxps32k358_lpspi_ops, s,
-                          TYPE_NXPS32K358_LPSPI, 0x400);
-    sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->mmio);
+    memory_region_init_io(&s->mmio, OBJECT(s), &nxps32k358_lpspi_ops, s,
+        TYPE_NXPS32K358_LPSPI, S32K_LPSPI_REG_MAX_OFFSET);
+    sysbus_init_mmio(sbd, &s->mmio);
 
-    sysbus_init_irq(SYS_BUS_DEVICE(obj), &s->irq);
-
-    s->ssi = ssi_create_bus(dev, "ssi");
+    s->ssi = ssi_create_bus(dev, "spi");
+    
+    sysbus_init_irq(sbd, &s->irq);
+    
+    s->cs_lines = g_new0(qemu_irq, s->num_cs_lines);
+    qdev_init_gpio_out_named(dev, s->cs_lines, "cs", s->num_cs_lines);
+    
+    fifo8_create(&s->tx_fifo, LPSPI_FIFO_BYTE_CAPACITY);
+    fifo8_create(&s->rx_fifo, LPSPI_FIFO_BYTE_CAPACITY);
 }
 
 static void nxps32k358_lpspi_class_init(ObjectClass *klass, const void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
-
+    dc->realize = nxps32k358_lpspi_realize;
     device_class_set_legacy_reset(dc, nxps32k358_lpspi_reset);
+    device_class_set_props(dc, nxps32k358_lpspi_properties);
     dc->vmsd = &vmstate_nxps32k358_lpspi;
 }
 
@@ -245,7 +326,6 @@ static const TypeInfo nxps32k358_lpspi_info = {
     .name = TYPE_NXPS32K358_LPSPI,
     .parent = TYPE_SYS_BUS_DEVICE,
     .instance_size = sizeof(NXPS32K358LPSPIState),
-    .instance_init = nxps32k358_lpspi_init,
     .class_init = nxps32k358_lpspi_class_init,
 };
 
